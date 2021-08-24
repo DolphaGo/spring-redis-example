036b51086b422a97904a6af538716cab3161349f
DolphaGo <adamdoha@naver.com>
2021-08-24 11:38:44 +0900

    init project

---
+++ redis-study2/src/main/java/dolphago/redis/RedisStudy2Application.java
@@ -0,0 +1,13 @@
+package dolphago.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisStudy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisStudy2Application.class, args);
+    }
+
+}
---
+++ redis-study2/src/test/java/dolphago/redis/RedisStudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dolphago.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisStudy2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
